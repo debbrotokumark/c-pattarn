a8e50c2eaff50d6c6c73c04ca11a06d687fbb95e
Debbroto Kumar Karmokar <136155528+debbrotokumark@users.noreply.github.com>
2023-06-16 11:57:35 +0600

    Add files via upload

---
+++ 26.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+   int a,b,c,d=1;
+scanf("%d",&a);
+
+  for(b=1;b<=a;b++)
+  {
+for(c=b;c<=a;c++){
+printf("%d ",b);
+d++;
+}
+printf("\n");
+
+}
+
+
+    return 0;
+}
